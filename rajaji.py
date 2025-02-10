7a1774b6b0c5cd8a144bb595f2b66f919e688962
Lancer0305 <lancerplays0305@outlook.com>
2025-02-10 15:45:28 +0530

    rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -2,7 +2,7 @@
 #SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
   
 #Enter Your Bot Token here get it from @botfather
-BOT_TOKEN=("7867420700:AAGSHURm1RlSAsryAitUB3_b0FQb3G_8qhM")
+BOT_TOKEN=("8155977907:AAETRQQyKdhhP6oHvew2gYx6XUeP0DRZfw0")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("@Sandipan_1508")
