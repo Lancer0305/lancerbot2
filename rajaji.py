1cd6a75bdf4c49cd01428f319328e1d88829ba9d
Lancer0305 <lancerplays0305@outlook.com>
2025-02-10 15:33:36 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfather
+BOT_TOKEN=("7867420700:AAGSHURm1RlSAsryAitUB3_b0FQb3G_8qhM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Sandipan_1508")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6228283651")
+
+
+
+
+
+  
